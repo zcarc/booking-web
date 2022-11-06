f0bd634973a1eaeca45038afb9f569dd5fa9f6ed
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-07 07:30:48 +0900

    Home 컴포넌트 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
