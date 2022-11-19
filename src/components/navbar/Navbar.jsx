56b97ae092f8ca2f8ccfa462cdd58626e938f44a
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-19 11:56:47 +0900

    navBar 로고 링크 추가

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Booking</span>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Booking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">가입하기</button>
           <button className="navButton">로그인</button>
