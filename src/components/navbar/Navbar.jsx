dd525855edef4799b044427f399b458dbba74264
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-20 15:56:42 +0900

    Navbar 로그인 상태에 따른 UI 수정

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 import "./navbar.css";
 
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">Booking</span>
         </Link>
-        <div className="navItems">
-          <button className="navButton">가입하기</button>
-          <button className="navButton">로그인</button>
-        </div>
+        {user ? (
+          user.username
+        ) : (
+          <div className="navItems">
+            <button className="navButton">가입하기</button>
+            <button className="navButton">로그인</button>
+          </div>
+        )}
       </div>
     </div>
   );
