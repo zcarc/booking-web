fdc5488cf09f554436ec33ecc4a91d27d35dfd79
Hyunsoo Lee <wrkbrs@gmail.com>
2022-11-20 15:56:03 +0900

    AuthContextProvider (index.js) 추가

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { SearchContextProvider } from "./components/context/SearchContext";
+import { AuthContextProvider } from "./components/context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <SearchContextProvider>
       <App />
     </SearchContextProvider>
-  </React.StrictMode>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
